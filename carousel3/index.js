841d028c5ea4dc01138d868ac9a77148c4427eae
guileal <guileal.dev@gmail.com>
2023-07-17 19:39:27 -0300

    Working with a problem when i've insert padding in carousel container

--- carousel3/index.js
+++ carousel3/index.js
@@ -4,7 +4,7 @@ let divInfos = document.getElementById('infos')
 let container = document.getElementById('container')
 let wrapperItems = document.getElementById('wrapper-items')
 let arrayItems = document.getElementsByClassName('list-item')
-
+let secondItemOffsetLeft 
 
 // Nav Btns  <- ->
 const previousButton = document.getElementById('previous')
@@ -21,44 +21,46 @@ function updateContentSize(){
     wrapper-items: ${wrapperItems.offsetWidth},
     items[0]: ${arrayItems[0].offsetWidth}`
    
-    // let
-    let secondItemOffset = arrayItems[1].offsetLeft - (parseInt(getComputedStyle(arrayItems[1]).marginLeft) + parseInt(getComputedStyle(arrayItems[0]).marginLeft));
+    secondItemOffsetLeft = arrayItems[1].offsetLeft - parseInt(getComputedStyle(arrayItems[1]).marginLeft)
     
     return {
-        secondItemOffset
+        secondItemOffsetLeft
     }
 }
 
 function previousUpdateArrayItems(){
+    // capture the first item content inside one variable-> tempItem
     let tempItem = arrayItems[0]
-    arrayItems[0].remove()
-    let lastItem = arrayItems.length - 1
 
+    // then remove it from html node. 
+    arrayItems[0].remove()
+
+    // get the last item in array
+    let lastItem = arrayItems.length - 1
     arrayItems[lastItem].after(tempItem)
 
     let marginLeft = parseInt(getComputedStyle(arrayItems[0]).marginLeft)
-    arrayItems[lastItem + 1].style.marginLeft = `${marginLeft}px`;
+    arrayItems[lastItem + 1].style.marginLeft = `${marginLeft}px`
     arrayItems[lastItem + 1].classList.remove('UI-fadeOut')
-    
 }
 
 
 // 🎉 Listeners
 previousButton.addEventListener('click', ()=>{
-    let {secondItemOffset} = updateContentSize()
-    arrayItems[0].style.marginLeft = `${-secondItemOffset}px`;
+    let {secondItemOffsetLeft} = updateContentSize()
+    arrayItems[0].style.marginLeft = `${-secondItemOffsetLeft}px`
 
     setTimeout(previousUpdateArrayItems,300)
 
     arrayItems[0].classList.add('UI-fadeOut')
-    arrayItems[lastItem + 1].classList.remove('UI-fadeIn')
-    // arrayItems[0].classList.add('UI-none')
+    // arrayItems[lastItem + 1].classList.remove('UI-fadeIn')
 })
 
 nextButton.addEventListener('click',()=>{
     console.log('Next Clicked')
 })
 
+// Update the layout ComputedStyle when the window is resized
 window.addEventListener('resize', function(){
     updateContentSize()
 })
